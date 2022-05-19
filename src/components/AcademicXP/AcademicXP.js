1830e1403bafc1484e725269d1851b63731a32b8
Marcos Leite <m.abner@live.co.uk>
2022-05-19 12:49:49 -0300

    reset

--- src/components/AcademicXP/AcademicXP.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class AcademicXP extends Component {
-  render() {
-    return <div>AcademicXP</div>;
-  }
-}
-
-export default AcademicXP;
--- src/components/WorkXP/WorkXP.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class WorkXP extends Component {
-  render() {
-    return <div>WorkXP</div>;
-  }
-}
-
-export default WorkXP;

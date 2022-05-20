8571eca3ce1582902340c86e569d26da968c3876
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-05-19 23:31:47 -0300

    temporary welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="app">
+		<router-view />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+	font-family: 'Raleway', sans-serif;
+	font-weight: 400;
+}
+
+.app {
+	min-height: 100vh;
+	position: relative;
+	background-color: #f1f1f1;
+}
+.container {
+	padding: 0 20px;
+	max-width: 1140px;
+	margin: 0 auto;
 }
 </style>

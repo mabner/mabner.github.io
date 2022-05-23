e1f008599496bdf1da521d339a7eb20a3a2a26d7
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-05-23 02:10:15 -0300

    contact form test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
 	<main class="about-page">
-		<h1>About</h1>
+		<h1>Sobre</h1>
 		<p>Website being developed as a portfolio to showcase my work and academic experience.</p>
 	</main>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,13 +2,29 @@
 	<div>
 		<main class="contact-page">
 			<h1>Contato</h1>
+			<form name="contact-me" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+				<input type="hidden" name="form-name" value="contact-me" />
+				<div class="form-group">
+					<label for="name">Nome</label>
+					<input type="text" name="name" id="name" class="form-control" placeholder="Nome" required>
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
+				</div>
+				<div class="form-group">
+					<label for="message">Mensagem</label>
+					<textarea name="message" id="message" class="form-control" placeholder="Mensagem" required></textarea>
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary">Enviar</button>
+				</div>
+			</form>
 		</main>
 	</div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
 </style>

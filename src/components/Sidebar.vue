51a73747f640d47bc5652bc36640ce7abd85eb9b
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-05-23 01:38:01 -0300

    responsive aside menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,6 +8,25 @@
 				<span class="material-icons">keyboard_double_arrow_right</span>
 			</button>
 		</div>
+		<h3>Menu</h3>
+		<div class="menu">
+			<router-link class="button" to="/">
+				<span class="material-icons">home</span>
+				<span class="text">Home</span>
+			</router-link>
+			<router-link class="button" to="/overview">
+				<span class="material-icons">work</span>
+				<span class="text">Resumo</span>
+			</router-link>
+			<router-link class="button" to="/about">
+				<span class="material-icons">help</span>
+				<span class="text">Sobre</span>
+			</router-link>
+			<router-link class="button" to="/contact">
+				<span class="material-icons">email</span>
+				<span class="text">Contato</span>
+			</router-link>
+		</div>
 	</aside>
 </template>
 
@@ -37,6 +56,9 @@ aside {
 
 		img {
 			width: 2rem;
+			border-style: outset;
+			border-width: 2px;
+			border-color: var(--light);
 			border-radius: 50%;
 		}
 	}
@@ -58,11 +80,59 @@ aside {
 				color: var(--light);
 				transition: 0.02s ease-out;
 			}
-			&:hover{
+
+			&:hover {
 				.material-icons {
 					color: var(--primary);
 					transform: translateX(0.5rem);
 				}
+			}
+		}
+	}
+
+	h3,
+	.button .text {
+		opacity: 0;
+		transition: 0.3s ease-out;
+	}
+
+	h3 {
+		color: var(--grey);
+		font-size: 0.875rem;
+		margin-bottom: 0.5rem;
+		text-transform: uppercase;
+	}
+
+	.menu {
+		margin: 0 -1rem;
+
+		.button {
+			display: flex;
+			align-items: center;
+			text-decoration: none;
+			padding: 0.5rem 1rem;
+			transition: 0.2s ease-out;
+
+			.material-icons {
+				font-size: 2rem;
+				color: var(--light);
+				transition: 0.2s ease-out;
+			}
+
+			.text {
+				color: var(--light);
+			}
+
+			&:hover, &.router-link-exact-active {
+				background-color: var(--dark-alt);
+
+				.material-icons,
+				.text {
+					color: var(--primary);
+				}
+			}
+			&.router-link-exact-active {
+				border-right: 5px solid var(--primary);
 			}
 		}
 	}
@@ -72,8 +142,20 @@ aside {
 
 		.menu-toggle-wrap {
 			top: -3rem;
+
 			.menu-toggle {
 				transform: rotate(-180deg);
+			}
+		}
+
+		h3,
+		.button .text {
+			opacity: 1;
+		}
+
+		.button {
+			.material-icons {
+				margin-right: 1rem;
 			}
 		}
 	}
